a5587811d5e36042d81e3f5169356dcc644ca608
Ksoft-Technologies2 <izzyeasy956@gmail.com>
2024-04-07 21:07:25 +0100

    cc

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const labelBtn = document.querySelector("#flexSwitchCheckChecked");
-
-labelBtn.addEventListener("change", function(){
-    var element = document.body
-    element.toggle.classList("dark-mode")
-})
